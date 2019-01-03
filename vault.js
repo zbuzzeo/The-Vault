ff329a1b81408f4c4795845a604af161fef1ecf5
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-02 18:46:28 -1000

    complete

--- vault.js
+++ vault.js
@@ -1,5 +1,23 @@
 'use strict';
 module.exports = function( ) {
-  
+    // let privateVar = 'lakdsksadlsdaklsad';
+    const vault = {  };
+    const setValue = ( key, value ) => {
+        vault[ key ] = value;
+    }
+    const getValue = ( key ) => {
+        // if vault[ key ] is some kind of truthey value then:
+        if ( vault[ key ] ) {
+            return vault[ key ];
+        }
+        else { return null };
+    }
+
+    return {
+        setValue: setValue,
+        getValue: getValue,
+    }
 
 };
+
+// console.log ( privateVar );
